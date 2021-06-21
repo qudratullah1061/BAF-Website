b4f2477b79c1b15bb3b8551c6573b58f2aa36c90
qudratullah1061 <32341027+qudratullah1061@users.noreply.github.com>
2021-06-21 23:16:38 +0500

    Added environment variables

--- src/app/components/homepage/slider/slider.component.ts
+++ src/app/components/homepage/slider/slider.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'baf-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+
+  baseUrl = environment.apiUrl;
 
   constructor() { }
 
