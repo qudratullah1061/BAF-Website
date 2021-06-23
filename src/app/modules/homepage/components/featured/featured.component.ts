b01d76095cd48aa3770fafdf6e92a6fb84d745cc
qudratullah1061 <32341027+qudratullah1061@users.noreply.github.com>
2021-06-23 19:30:30 +0500

    scss added.

--- src/app/modules/homepage/components/featured/featured.component.ts
+++ src/app/modules/homepage/components/featured/featured.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IHomepageFeaturedItems } from '../../models/IHomepage-featured-items';
 
 @Component({
   selector: 'baf-featured',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./featured.component.scss']
 })
 export class FeaturedComponent implements OnInit {
+
+@Input() homePageFeaturedItems: IHomepageFeaturedItems[]
 
   constructor() { }
 
