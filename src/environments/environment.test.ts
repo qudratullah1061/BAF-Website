8f6883e0f74d1fcdf2a5904408ad906a9bb5536b
qudratullah1061 <32341027+qudratullah1061@users.noreply.github.com>
2021-06-24 21:44:24 +0500

    header changes.

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "https://baf-website4-test.itcomrade.us/"
+  apiUrl: "https://baf-website4-test.itcomrade.us/",
+  show_lang_btn:false
 };
