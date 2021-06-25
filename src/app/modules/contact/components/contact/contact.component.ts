ed42b0f35df03610100be9cc7cf703e976cd5f38
rao-officials <rao.officials@gmail.com>
2021-06-25 17:15:29 +0500

    integrate contact & About page

--- src/app/modules/contact/components/contact/contact.component.ts
+++ src/app/modules/contact/components/contact/contact.component.ts
@@ -9,13 +9,15 @@ import { ContactService } from '../../services/contact.service';
 })
 export class ContactComponent implements OnInit {
 
-  contactPageData: Icontact[]
+  contactPageData: Icontact[] = [];
+  public items: any = [1,2,3,4,5]
 
   constructor(private contactPagesService: ContactService) { }
 
   ngOnInit(): void {
     this.contactPagesService.getContactPageData().subscribe({
       next: data => {
+        
         this.contactPageData = data.description as Icontact[];
         console.log(this.contactPageData);
       }
