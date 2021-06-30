270ffb50b53378a8e01b53914819e807cc5ccfae
rao-officials <rao.officials@gmail.com>
2021-06-30 15:43:11 +0500

    add 404 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,17 @@ import { StoriesComponent } from './modules/stories/components/stories/stories.c
 import { AboutComponent } from './modules/about/components/about/about.component';
 import { ContactComponent } from './modules/contact/components/contact/contact.component';
 import { CollectionDetailComponent } from '@collections/components/detail/collection-detail.component';
+import { PageNotFoundComponent } from '@shared/components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   { 'path': '', component: HomepageComponent },
-  { 'path': 'collections', component: CollectionsComponent},
+  { 'path': 'collections', component: CollectionsComponent },
   { 'path': 'collection/:code', component: CollectionDetailComponent },
   { 'path': 'stories', component: StoriesComponent },
   { 'path': 'about', component: AboutComponent },
   { 'path': 'contact', component: ContactComponent },
-  { 'path': '**', redirectTo: '', pathMatch: 'full' }
-
+  { 'path': '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NumberIteratorPipe } from './pipes/number-iterator.pipe';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
 
 
-    NumberIteratorPipe
+    NumberIteratorPipe,
+        PageNotFoundComponent
   ],
   imports: [
     CommonModule
